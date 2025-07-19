6132ac80c23c9101a8734412be5ed801cd1bf748
EmacsIsBetterThanVi <168662379+EmacsIsBetterThanVi@users.noreply.github.com>
2025-07-19 12:49:33 +0000

    Various Updates

---
+++ PythonEmulator/__init__.py
@@ -0,0 +1,1 @@
+from EmulatorCore import * # pyright: ignore
--- PythonEmulator/riscv.py
+++ PythonEmulator/riscv.py
@@ -1,5 +1,11 @@
 from EmulatorCore import register, Ram
 class riscv32():
+  rtype = []
+  itype = []
+  stype = []
+  btype = []
+  utype = []
+  jtype = []
   def __init__(self, ram):
     self.registers = [register(0, lock=True)]
     for i in range(31):
@@ -7,9 +13,17 @@ class riscv32():
     self.pc = register()
     self.ram = ram
   # Execute the next instruction in memory, then increment the program counter by 4
-  def tick(self):
+  def decode(self):
     byte1 = self.ram.read(self.pc.get())
     byte2 = self.ram.read(self.pc.get()+1)
     byte3 = self.ram.read(self.pc.get()+2)
     byte4 = self.ram.read(self.pc.get()+3)
     self.pc += 4
+    opcode = byte1 & 0b111111
+    rd = 0
+    rs1 = 0
+    rs2 = 0
+    funct3 = 0
+    funct7 = 0
+    imm = 0
+    if opcode in rtype:
