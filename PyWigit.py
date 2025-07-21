8de3b58cccfc001d0b0794969b866fa598f3a7e9
EmacsIsBetterThanVi <168662379+EmacsIsBetterThanVi@users.noreply.github.com>
2025-07-21 00:29:11 +0000

    Updated PyWigit and the frontend

--- PyWigit.py
+++ PyWigit.py
@@ -1,70 +1,6 @@
 import pygame
-pygame.init()
-pygame.font.init()
-clock = pygame.time.Clock()
-class Button():
-    def __init__(self, display, LeftClick=None, RightClick=None, Mouse=None):
-        self.display = display
-        self.LeftClick=LeftClick
-        self.RightClick=RightClick
-        self.Mouse=Mouse
-    def draw(self, screen, loc):
-        screen.blit(self.display, loc)
-    def Click(self, event):
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            if self.display.collidepoint(event.pos):
-                if event.button == 1 and self.LeftClick:
-                    self.LeftClick()
-                elif event.button == 2 and self.RightClick:
-                    self.RightClick()
-                elif event.button == 3 and self.Mouse:
-                    self.Mouse()
-class ImageButton(Button):
-    def __init__(self, name, LeftClick=None, RightClick=None, Mouse=None):
-        self.display = pygame.image.load_extended(name)
-        self.LeftClick=LeftClick
-        self.RightClick=RightClick
-        self.Mouse=Mouse
-def NewScreen(size):
-    return pygame.display.set_mode(size)
-def NewFont(name, size):
-    return pygame.font.SysFont(name, size)
-def SetCaption(caption):
-    pygame.display.set_caption(caption)
-MultiScrn = []
-Windows = []
-running = True
-def CreateScrn(DrawFunc, InputFunc, color):
-    MultiScrn.append({"Draw":DrawFunc, "Input":InputFunc, "color":color})
-class Screen():
-  def __init__(self, screen,x=0,y=0, size=(400, 400), FullScreen=False):
-    self.CurentScrn = 0
-    if FullScreen:
-      self.screen = screen
-      self.x = 0
-      self.y = 0
-    else:
-      self.screen = pygame.Surface(size)
-      self.x = x
-      self.y = y
-    Windows.append(self)
-  def MainLoop(self):
-    self.screen.fill(MultiScrn[self.CurentScrn]["color"])
-    MultiScrn[self.CurentScrn]["Draw"](self.screen)
-  def ChangeScrn(self, num):
-    self.CurentScrn = num
-  def Close(self):
-    if len(Windows) == 1:
-      global running
-      running = False
-    else:
-      Windows.remove(self)
-      del self
-def Status():
-    return running
-Enter=13
-Escape=27
-BackSpace=8
+import glob
+from os import getcwd
 White = (255, 255, 255)
 Black = (0, 0, 0)
 Red = (255, 0, 0)
@@ -80,20 +16,259 @@ Orange = (255, 128, 0)
 Purple = (128, 0, 255)
 Pink = (255, 0, 128)
 Brown = (128, 64, 0)
-def MainLoop(screen):
+pygame.init()
+pygame.font.init()
+clock = pygame.time.Clock()
+class Pointer():
+  def __init__(self, value):
+    self.value = value
+  def __iadd__(self, other):
+    self.value += other
+    return self
+  def __isub__(self, other):
+    self.value -= other
+    return self
+  def __imul__(self, other):
+    self.value *= other
+    return self
+  def __itruediv__(self, other):
+    self.value //= other
+    return self
+  def __imod__(self, other):
+    self.value %= other
+    return self
+  def __iand__(self, other):
+    self.value &= other
+    return self
+  def __ior__(self, other):
+    self.value |= other
+    return self
+  def __ixor__(self, other):
+    self.value ^= other
+    return self
+  def __ilshift__(self, other):
+    self.value <<= other
+    return self
+  def __irshift__(self, other):
+    self.value >>= other
+    return self
+  def __eq__(self, other):
+    return self.value == other
+  def __ne__(self, other):
+    return self.value != other
+  def __lt__(self, other):
+    return self.value < other
+  def __le__(self, other):
+    return self.value <= other
+  def __gt__(self, other):
+    return self.value > other
+  def __ge__(self, other):
+    return self.value >= other
+  def __add__(self, other):
+    return self.value + other
+  def __sub__(self, other):
+    return self.value - other
+  def __mul__(self, other):
+    return self.value * other
+  def __truediv__(self, other):
+    return self.value // other
+  def __mod__(self, other):
+    return self.value % other
+  def __and__(self, other):
+    return self.value & other
+  def __or__(self, other):
+    return self.value | other
+  def __xor__(self, other):
+    return self.value ^ other
+  def __lshift__(self, other):
+    return self.value << other
+  def __rshift__(self, other):
+    return self.value >> other
+  def __int__(self):
+    return self.value
+  def __str__(self):
+    return str(self.value)
+  def __repr__(self):
+    return repr(self.value)
+  def __hash__(self):
+    return hash(self.value)
+  def __bool__(self):
+    return bool(self.value)
+  def __len__(self):
+    return len(self.value)
+  def __getitem__(self, key):
+    return self.value[key]
+  def __setitem__(self, key, value):
+    self.value[key] = value
+  def __delitem__(self, key):
+    del self.value[key]
+  def __iter__(self):
+    return iter(self.value)
+  def __reversed__(self):
+    return reversed(self.value)
+  def __contains__(self, item):
+    return item in self.value
+  def __getattr__(self, name):
+    return getattr(self.value, name)
+  def __setattr__(self, name, value):
+    if name == "value":
+      self.__dict__[name] = value
+    else:
+      setattr(self.value, name, value)
+  def __delattr__(self, name):
+    delattr(self.value, name)
+class Button():
+    def __init__(self, display, LeftClick=None, RightClick=None, Mouse=None):
+        self.display = display
+        self.LeftClick=LeftClick
+        self.RightClick=RightClick
+        self.Mouse=Mouse
+        self.rect = self.display.get_rect()
+    def draw(self, screen, loc):
+        self.rect = screen.blit(self.display, loc)
+    def Click(self, event):
+      if event.type == pygame.MOUSEBUTTONDOWN:
+        if self.rect.collidepoint(event.pos):
+          if event.button == 1 and self.LeftClick:
+            self.LeftClick()
+          elif event.button == 2 and self.RightClick:
+            self.RightClick()
+          elif event.button == 3 and self.Mouse:
+            self.Mouse()
+          return True
+      return False
+class ImageButton(Button):
+    def __init__(self, name, LeftClick=None, RightClick=None, Mouse=None):
+        self.display = pygame.image.load_extended(name)
+        self.LeftClick=LeftClick
+        self.RightClick=RightClick
+        self.Mouse=Mouse
+def NewScreen(size):
+    return pygame.display.set_mode(size)
+def NewFont(name, size):
+    return pygame.font.SysFont(name, size)
+def SetCaption(caption):
+    pygame.display.set_caption(caption)
+Windows = []
+running = True
+def CreateScrn(DrawFunc, InputFunc, color, FastTick=None):
+    if FastTick:
+      MultiScrn.append({"Draw":DrawFunc, "Input":InputFunc, "color":color, "FastTick":FastTick})
+    else:
+      MultiScrn.append({"Draw":DrawFunc, "Input":InputFunc, "color":color})
+class Screen():
+  def __init__(self, screen,x=0,y=0, size=(400, 400), FullScreen=False):
+    self.CurentScrn = 1 # Screen starts on screen 1
+    if FullScreen:
+      self.screen = screen
+      self.x = 0
+      self.y = 0
+    else:
+      self.screen = pygame.Surface(size)
+      self.x = x
+      self.y = y
+    Windows.append(self)
+  def Draw(self):
+    self.screen.fill(MultiScrn[self.CurentScrn]["color"])
+    MultiScrn[self.CurentScrn]["Draw"](self.screen)
+  def ChangeScrn(self, num):
+    self.CurentScrn = num
+  def Close(self):
+    if len(Windows) == 1:
+      global running
+      running = False
+    else:
+      Windows.remove(self)
+      del self
+def Status():
+    return running
+class FileChoser(Screen):
+  @staticmethod
+  def Open(title, returnv, filetypes="*.*"):
+    FileChoser(title, pygame.font.SysFont("timesnewroman", 20), returnv, filetypes)
+  def __init__(self, title, font, returnv, filetypes="*.*"):
+    self.title = title
+    self.filetypes = filetypes
+    self.file = ""
+    self.font = font
+    self.screen = pygame.Surface((400, 400))
+    self.path = getcwd()
+    self.scroll = 0
+    self.x = 0
+    self.y = 0
+    Windows.append(self)
+    self.returnv = returnv
+    self.CurentScrn = 0 # FileChoser is always screen 0
+    self.closeButton = Button(self.font.render("Close", True, White))
+    MultiScrn[0]["Object"] = self
+  def Draw(self):
+    self.screen.fill(Black)
+    self.closeButton.draw(self.screen, (200, 10))
+    self.screen.blit(self.font.render(self.title, True, White), (10, 10))
+    if self.file:
+      self.screen.blit(self.font.render(self.file, True, White), (10, 40))
+    self.screen.blit(self.font.render(self.path, True, White), (10, 70))
+    files = glob.glob(self.path + "/" + self.filetypes)
+    for i in range(len(files)):
+      if i>=self.scroll and i<self.scroll+10:
+        self.screen.blit(self.font.render(files[i], True, White), (10, 100 + i * 30))
+  def Events(self,event):
+    if event.type == pygame.MOUSEBUTTONDOWN:
+      if event.button == 4:
+        self.scroll -= 1
+      elif event.button == 5:
+        self.scroll += 1
+      elif event.button == 1:
+        files = glob.glob(self.path + "/" + self.filetypes)
+        if self.closeButton.Click(event):
+          self.returnv.value = self.file
+          self.Close()
+          return True
+        for i in range(len(files)):
+          if i>=self.scroll and i<self.scroll+10:
+            if pygame.Rect(10, 100 + i * 30, 400, 30).collidepoint(event.pos):
+              self.file = files[i]
+              return True
+    return False
+  @staticmethod
+  def Default():
+    return FileChoser("File Choser", pygame.font.SysFont("timesnewroman", 20), Pointer(""))
+  def ChangeScrn(self, num):
+    pass
+MultiScrn = [{"Object":None, "color":White}]
+FileChoser.Default()
+Windows = []
+FastTicks = 0
+def MainLoop(screen, Fast=False):
   global running
   for event in pygame.event.get():
-      if event.type == pygame.QUIT:
-        running = False
-      else:
-        handled = False
-        for window in Windows:
+      handled = False
+      for window in Windows:
+        if MultiScrn[window.CurentScrn].get("Object"):
+          handled = MultiScrn[window.CurentScrn]["Object"].Events(event)
+        else:
           handled = MultiScrn[window.CurentScrn]["Input"](event)
-          if handled:
-            break
-  screen.fill(Black)
+        if handled:
+          break
+      if event.type == pygame.QUIT and not handled:
+        running = False
+  shouldTick=False
   for window in Windows:
-    window.MainLoop()
-    screen.blit(window.screen, (window.x, window.y))
-  pygame.display.flip()
-  clock.tick(60)
+    if MultiScrn[window.CurentScrn].get("FastTick"):
+      MultiScrn[window.CurentScrn]["FastTick"]()
+  if not Fast:
+    shouldTick=True
+  else:
+    global FastTicks
+    FastTicks+=1
+    if FastTicks>=20:
+      FastTicks=0
+      shouldTick=True
+  if shouldTick:
+    screen.fill(Black)
+    for window in Windows:
+      window.Draw()
+      screen.blit(window.screen, (window.x, window.y))
+      pygame.draw.rect(screen, White, pygame.Rect(window.x, window.y, window.screen.get_width(), window.screen.get_height()), 1)
+    pygame.display.flip()
+    clock.tick(60)
--- main.py
+++ main.py
@@ -1,24 +1,71 @@
 # Importing CORE, which previously was imported as using from CORE import *. This is because I am using replit and it complains about star imports. The first line is to prevent errors, while the second line is to silence the warnings.
 from PyWigit import * # pyright: ignore
-from PyWigit import NewScreen, SetCaption, CreateScrn, MainLoop, Screen, Status, pygame, Black, White, Windows, Button, NewFont
-from PythonEmulator import Ram
+from PyWigit import NewScreen, SetCaption, CreateScrn, MainLoop, Screen, Status, pygame, Black, White, Button, NewFont, FileChoser
+from PythonEmulator import Ram, Memory
+from PythonEmulator.display import Console
+from PythonEmulator.input import Keyboard
 from PythonEmulator.riscv import riscv32
+cpu: riscv32=None # pyright: ignore
+ram: Memory=None # pyright: ignore
+console: Console=None # pyright: ignore
+keyboard: Keyboard = None # pyright: ignore
+RomFile: Pointer = Pointer("")
 def StartEmulator():
-    EmulatorScreen.ChangeScrn(1)
-    ram = Ram(65536)
-    cpu = riscv32(ram)
-screen = NewScreen((600, 400))
+  global ram, cpu, console, keyboard
+  # Configuring the emulator
+  ram = Memory([(0x00100000,Ram(2^24))], 2**32-1)
+  cpu = riscv32(ram)
+  console = Console(80, 25)
+  keyboard = Keyboard("ascii")
+  # If a rom file is specified, load it, treating it as ram, but with a locked flag set to true, so it can't be modified.
+  if RomFile.value != "":
+    ROM = bytearray(open(RomFile.value, "rb").read())
+    ram.mm(0x0, Ram(0x10000, ROM, True))
+  # Our emulator will be a riscv32 emulator, with 64kb of ram, and a console with 80x25 characters
+  # Because riscv uses memory mapped io, here is the memory map:
+  # 0x00000000 - 0x0000FFFF: Boot ROM
+  # 0x00010000 - 0x000108CF: MMIO
+  #    0x00010000 - 0x0001000F: Keyboard
+  #    0x00010010 - 0x0001001F: Disk
+  #    0x00010040 - 0x000100FF: Reserved
+  #    0x00010100 - 0x000108CF: Console
+  # 0x00020000 - 0x000FFFFF: Secondary ROM
+  # 0x00100000 - 0x001FFFFF: RAM
+  # 0x00200000 - 0xFFFFFFFF: Reserved
+  # Map the console and Keyboard to the memory map
+  ram.mmio(0x10100, console)
+  ram.mmio(0x10000, keyboard)
+  # Change the screen to the emulator screen, starting it
+  EmulatorScreen.ChangeScrn(2)
+screen = NewScreen((640, 400))
 EmulatorScreen = Screen(screen, FullScreen=True)
 SetCaption("Python Emulator")
-# TODO: Define how each screen should look and behave
-# This screen handles the main menu
 font = NewFont("timesnewroman", 20)
+# This screen handles the main menu
 LaunchButton = Button(font.render("Launch", True, White), LeftClick=StartEmulator)
+SelectBootRomButton = Button(font.render("Select Boot Rom", True, White), LeftClick=lambda : FileChoser.Open("Select Boot Rom", RomFile, "*.bin"))
 def DrawScreen0(screen):
     LaunchButton.draw(screen, (100, 100))
+    SelectBootRomButton.draw(screen, (100, 150))
 def Screen0Events(event):
+    if LaunchButton.Click(event):
+      return True
+    if SelectBootRomButton.Click(event):
+      return True
     return False
 CreateScrn(DrawScreen0, Screen0Events, Black)
+# This screen handles the emulator
+def DrawScreen1(screen):
+  console.draw(screen)
+def Screen1Events(event):
+  if event.type == pygame.KEYDOWN:
+    keyboard.press(event.key)
+    return True
+  return False
+def Screen1FastTick():
+  ram.mmioHandle()
+  cpu.execute()
+CreateScrn(DrawScreen1, Screen1Events, Black, Screen1FastTick)
 while Status():
   MainLoop(screen)
 pygame.quit()
